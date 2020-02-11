60beed6a9e04ac95beb9a355e08f3a347ef7ac37
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-02-12 01:03:45 +0900

    파인더 IE 에러를 바로잡기 위해 polyfill 패키지를 추가합니다.

--- finder-src/src/index.js
+++ finder-src/src/index.js
@@ -1,6 +1,9 @@
+import "core-js/stable"
+import "regenerator-runtime/runtime"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import 'react-app-polyfill/ie11'
 
 const $ = document.querySelector.bind(document)
 
